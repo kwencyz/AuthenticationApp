ebae532017587b7acdba1168aded20162aa40335
Haziq Ruslan <haziq.ruslan@fiuu.com>
2026-09-18 17:31:12 +0800

    Refactor App component to use AppNavigator and AuthProvider for improved navigation and authentication management

--- App.tsx
+++ App.tsx
@@ -5,22 +5,13 @@
  * @format
  */
 
-import { StyleSheet } from 'react-native';
-import LoginScreen from './src/screens/LoginScreen';
-
-import SignupScreen from './src/screens/SignupScreen';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-const Stack = createNativeStackNavigator();
+import { AuthProvider } from './src/auth/AuthContext';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Signup" component={SignupScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthProvider>
+      <AppNavigator />
+    </AuthProvider>
   )
 }
